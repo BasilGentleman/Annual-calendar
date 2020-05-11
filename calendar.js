1d9b5a961b3ccd80dbcd918399a3375cf09a33f6
Bsil <364196423@qq.com>
2020-05-11 20:03:31 +0800

    first commit

---
+++ calendar.js
@@ -0,0 +1,36 @@
+ //定义年日历生成函数
+function calendar(y) {
+
+	//获取指定年份1月1日的星期数值
+	var w = new Date(y,0).getDay();
+
+	var html = '<div class ="box">';
+
+	//拼接每个月份表格
+	for (var m = 1; m <= 12 ; m++) {
+		html += '<table>';
+		html += '<tr class="title"><th colspan="7">'+ y + '年' + m + '月</th></tr>';
+		html += '<tr><td>日</td><td>一</td><td>二</td><td>三</td><td>四</td><td>五</td><td>六</td></tr>';
+
+		//获取当前月份共有多少天
+		var max = new Date(y,m,0).getDate();
+
+		html += '<tr>';
+			for (var d = 1; d <= max; d++) {
+				if (w && d == 1) {
+					html += '<td colspan = "' + w +'"> </td>';  //如果当月第一天不为周日，则填充空白
+				}
+				html += '<td>' + d + '</td>';
+				if ( w == 6 && d != max ) {
+					html += '</tr><tr>';
+				} else if ( d == max ) {
+					html += '</tr>';
+				}
+				w = ( w + 1 > 6 ) ? 0 : w + 1; 
+			}
+
+			html += '</table>';
+		}
+		html += '</div>';
+		return html;
+}
